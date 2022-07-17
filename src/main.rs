80ea31b38fb514a263a98a77aafca9b6ce778c07
Josh <josh.hewlett@gmail.com>
2022-07-17 00:35:07 -0700

    Drawable abstraction, collision detection

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-use std::time::{Duration, SystemTime};
+use std::collections::HashSet;
+use std::time::Duration;
 
 use sdl2;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::keyboard::Keycode::P;
 use sdl2::pixels::Color;
 use sdl2::rect::Point;
-use sdl2::render::{Canvas, WindowCanvas};
+use sdl2::render::WindowCanvas;
 
 static WINDOW_WIDTH: u32 = 800;
 static WINDOW_HEIGHT: u32 = 600;
+
+static CENTER_X: i32 = (WINDOW_WIDTH / 2) as i32;
+static CENTER_Y: i32 = (WINDOW_HEIGHT / 2) as i32;
 
 enum Direction {
     Up,
@@ -34,9 +37,7 @@ struct Shape {
 // TODO: Should Shape implement Iterable?
 impl Shape {
     fn new(shape: Vec<Vec<Option<Color>>>) -> Self {
-        Shape {
-            shape,
-        }
+        Shape { shape }
     }
 
     // fn shape_data(&self) -> &Vec<Vec<Option<Color>>> {
@@ -67,6 +68,14 @@ impl Default for Shape {
                  Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE)],
             vec![Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE),
                  Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE)],
+            vec![Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE),
+                 Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE)],
+            vec![Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE),
+                 Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE)],
+            vec![Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE),
+                 Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE)],
+            vec![Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE),
+                 Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE), Some(Color::WHITE)],
         ];
 
         Shape::new(shape)
@@ -76,50 +85,98 @@ impl Default for Shape {
 ///
 /// Drawable definition
 ///
-trait Drawable: {
+trait Drawable {
     fn position(&self) -> &Point;
+    fn set_position(&mut self, point: Point);
     fn shape(&self) -> &Shape;
 
-    fn draw(&self, canvas: &mut WindowCanvas) {
+    // TODO: rename and implement memoization somehow
+    fn get_active_pixels(&self) -> Vec<(Point, &Option<Color>)> {
         let position: &Point = &self.position();
         let x_origin = position.x;
         let y_origin = position.y;
 
         let shape: &Shape = &self.shape();
 
+        let mut result: Vec<(Point, &Option<Color>)> = Vec::new();
+
         for y_ptr in 0..shape.height() {
             for x_ptr in 0..shape.width() {
                 let pixel: &Option<Color> = shape.get_pixel(x_ptr, y_ptr);
 
                 if pixel.is_some() {
-                    &canvas.set_draw_color(pixel.unwrap());
-                    &canvas.draw_point(Point::new(x_origin + i32::try_from(x_ptr).unwrap(), y_origin + i32::try_from(y_ptr).unwrap()));
+                    let point = Point::new(
+                        x_origin + i32::try_from(x_ptr).unwrap(),
+                        y_origin + i32::try_from(y_ptr).unwrap(),
+                    );
+                    result.push((point, pixel));
                 }
             }
         }
+
+        result
+    }
+
+    fn draw(&self, canvas: &mut WindowCanvas) {
+        let pixels: Vec<(Point, &Option<Color>)> = self.get_active_pixels();
+
+        pixels.iter().for_each(|pixel| {
+            canvas.set_draw_color(pixel.1.unwrap());
+            canvas.draw_point(pixel.0).unwrap();
+        });
     }
 }
 
 ///
 /// Collidable definition
 ///
-// TODO
-trait Collidable {}
+trait Collidable: Drawable {
+    fn has_collided(&self, other: &dyn Collidable) -> bool {
+        let common_points: HashSet<Point> = self
+            .get_active_pixels()
+            .iter()
+            .map(|pixel| pixel.0)
+            .collect();
 
-///
-/// Collidable definition
-///
-// TODO
+        other
+            .get_active_pixels()
+            .iter()
+            .map(|p| p.0)
+            .any(|p| common_points.contains(&p))
+    }
+}
 
 ///
 /// Moveable definition
 ///
-trait Moveable : Collidable {
+trait Moveable: Drawable {
     fn direction(&self) -> &Direction;
-    fn apply_movement(&mut self);
     fn change_direction(&mut self, new_direction: Direction);
+
+    fn apply_movement(&mut self) {
+
+        let new_position: Point;
+        match &self.direction() {
+            Direction::Up => {
+                new_position = Point::new(self.position().x, self.position().y - 1);
+            }
+            Direction::Down => {
+                new_position = Point::new(self.position().x, self.position().y + 1);
+            }
+            Direction::Left => {
+                new_position = Point::new(self.position().x - 1, self.position().y);
+            }
+            Direction::Right => {
+                new_position = Point::new(self.position().x + 1, self.position().y);
+            }
+        }
+
+        self.set_position(new_position);
+    }
 }
 
+///
+/// ===== Entities =====
 ///
 /// Player definition
 ///
@@ -130,9 +187,9 @@ struct Player {
 }
 
 impl Player {
-    fn new(point: Point) -> Self {
+    fn new(position: Point) -> Self {
         Player {
-            position: point,
+            position,
             shape: Shape::default(),
             current_direction: Direction::default(),
         }
@@ -141,13 +198,17 @@ impl Player {
 
 impl Default for Player {
     fn default() -> Self {
-        Player::new(Point::new(0, 0))
+        Player::new(Point::new(CENTER_X, CENTER_Y))
     }
 }
 
 impl Drawable for Player {
     fn position(&self) -> &Point {
         &self.position
+    }
+
+    fn set_position(&mut self, new_position: Point) {
+        self.position = new_position;
     }
 
     fn shape(&self) -> &Shape {
@@ -165,34 +226,58 @@ impl Moveable for Player {
     fn change_direction(&mut self, new_direction: Direction) {
         self.current_direction = new_direction;
     }
+}
 
-    fn apply_movement(&mut self) {
-        // TODO: Implement default apply_movement method
-        match &self.current_direction {
-            Direction::Up => {
-                self.position.y -= 1;
-            }
-            Direction::Down => {
-                self.position.y += 1;
-            }
-            Direction::Left => {
-                self.position.x -= 1;
-            }
-            Direction::Right => {
-                self.position.x += 1;
-            }
+///
+/// Coin definition
+///
+
+struct Coin {
+    position: Point,
+    shape: Shape,
+}
+
+impl Coin {
+    fn new(position: Point) -> Self {
+        Coin {
+            position,
+            shape: Shape::default(),
         }
     }
 }
 
+impl Default for Coin {
+    fn default() -> Self {
+        Coin::new(Point::new(CENTER_X + 50, CENTER_Y + 50))
+    }
+}
+
+// TODO: This is probably a good use of #derive!
+impl Drawable for Coin {
+    fn position(&self) -> &Point {
+        &self.position
+    }
+
+    fn set_position(&mut self, new_position: Point) {
+        self.position = new_position;
+    }
+
+    fn shape(&self) -> &Shape {
+        &self.shape
+    }
+}
+
+impl Collidable for Coin {}
+
 ///
-/// Main program
+/// ===== Main Program ======
 ///
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("PacMan (Maybe Snake)", WINDOW_WIDTH, WINDOW_HEIGHT)
+    let window = video_subsystem
+        .window("PacMan (Maybe Snake (idk rn))", WINDOW_WIDTH, WINDOW_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
@@ -201,31 +286,47 @@ pub fn main() {
     let mut event_pump = sdl_context.event_pump().unwrap();
 
     let mut player = Player::default();
+    let coin = Coin::default();
 
     let fps = 60;
     let frame_duration = 1_000_000_000u32 / fps;
     'running: loop {
-        let now = SystemTime::now();
+        // let now = SystemTime::now();
 
         // TODO: Test if event stack pushes more than once a frame
         // Check for events
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. } |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
                     break 'running;
                 }
-                Event::KeyDown { keycode: Some(Keycode::I), .. } => {
-                    &player.change_direction(Direction::Up);
+                Event::KeyDown {
+                    keycode: Some(Keycode::I),
+                    ..
+                } => {
+                    player.change_direction(Direction::Up);
                 }
-                Event::KeyDown { keycode: Some(Keycode::J), .. } => {
-                    &player.change_direction(Direction::Left);
+                Event::KeyDown {
+                    keycode: Some(Keycode::J),
+                    ..
+                } => {
+                    player.change_direction(Direction::Left);
                 }
-                Event::KeyDown { keycode: Some(Keycode::K), .. } => {
-                    &player.change_direction(Direction::Down);
+                Event::KeyDown {
+                    keycode: Some(Keycode::K),
+                    ..
+                } => {
+                    player.change_direction(Direction::Down);
                 }
-                Event::KeyDown { keycode: Some(Keycode::L), .. } => {
-                    &player.change_direction(Direction::Right);
+                Event::KeyDown {
+                    keycode: Some(Keycode::L),
+                    ..
+                } => {
+                    player.change_direction(Direction::Right);
                 }
                 _ => {}
             }
@@ -236,51 +337,27 @@ pub fn main() {
         canvas.clear();
 
         // The rest of the game loop goes here...
-        // apply_direction(&mut player_position, &direction);
-        // draw_square(&mut canvas, Color::WHITE, &player_position, 20);
-        // draw_triangle(&mut canvas, Color::WHITE, &player_position, 60);
+        player.draw(&mut canvas);
 
-        &player.draw(&mut canvas);
+        if player.has_collided(&coin) {
+            println!("Player collected coin!");
+            break 'running;
+        } else {
+            coin.draw(&mut canvas);
+        }
 
-        &player.apply_movement();
+        player.apply_movement();
 
         canvas.present();
 
         // Log how long the frame processing took
-        let elapsed = now.elapsed().unwrap().as_nanos();
-        let percent_time_to_process = (elapsed as f64 / frame_duration as f64) * 100 as f64;
-        println!("Percent of frame to process: {}%", percent_time_to_process);
-        println!("Elapsed time: {}", elapsed);
-        let remaining_frame_duration =
-            if elapsed > frame_duration as u128 { 0 } else { frame_duration - u32::try_from(elapsed).unwrap_or(0) };
-        ::std::thread::sleep(Duration::new(0, remaining_frame_duration));
+        // let elapsed = now.elapsed().unwrap().as_nanos();
+        // let percent_time_to_process = (elapsed as f64 / frame_duration as f64) * 100 as f64;
+        // println!("Percent of frame to process: {}%", percent_time_to_process);
+        // println!("Elapsed time: {}", elapsed);
+        // let remaining_frame_duration =
+        //     if elapsed > frame_duration as u128 { 0 } else { frame_duration - u32::try_from(elapsed).unwrap_or(0) };
+        // ::std::thread::sleep(Duration::new(0, remaining_frame_duration));
+        ::std::thread::sleep(Duration::new(0, frame_duration));
     }
 }
-
-//
-// fn draw_square(canvas: &mut WindowCanvas, color: Color, origin: &Point, width: u32) {
-//     &canvas.set_draw_color(color);
-//
-//
-//     for y_pos in origin.y..(origin.y + width as i32) {
-//         for x_pos in origin.x..(origin.x + width as i32) {
-//             &canvas.draw_point(Point::new(x_pos, y_pos));
-//         }
-//     }
-// }
-//
-// fn draw_triangle(canvas: &mut WindowCanvas, color: Color, origin: &Point, height: u32) {
-//     &canvas.set_draw_color(color);
-//
-//     let mut line_width = 1;
-//
-//     for y_pos in origin.y..(origin.y + height as i32) {
-//         for x_pos in (origin.x - (line_width / 2))..(origin.x + (line_width / 2)) {
-//             &canvas.draw_point(Point::new(x_pos, y_pos));
-//         }
-//
-//         if (y_pos % 2 == 0) {
-//             line_width += 2;
-//         }
-//     }
-// }
